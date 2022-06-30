6b0dd08ec46123cb3b8c919ed78264490126721c
galxy25 <levismschoen@gmail.com>
2022-06-29 17:59:59 -0700

    add skeleton for daemon and interactive kava health monitoring program development and testing

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # doctor
-Kava application and node infrastructure health monitoring daemon with pluggable metric collection backends
+
+Kava application and node infrastructure health monitoring daemon with configurable metric collection backends (e.g. stdout, file or AWS Cloudwatch).
+
+## Usage
+
+### Interactive Mode
+
+![Work in Progress](./docs/imgs/doctor-interactive-mode.png)
+
+### Daemon Mode
+
+```bash
+$ KAVA_RPC_URL=https://rpc.public.us-west.testing.kava.io:443/ doctor
+
+synched up to block 484800 @ 1656543494
+synched up to block 484801 @ 1656543499
+synched up to block 484802 @ 1656543504
+```
+
+## Development
+
+### Dependencies
+
+### Configuration
+
+### Building
+
+### Running
+
+### Testing
+
+### Publishing
---
+++ docker/supervisord/start-services.sh
@@ -0,0 +1,64 @@
+#!/usr/bin/env bash
+
+export START_KAVA_NODE=true
+
+if [ "$MODE" = "offline" ]
+then
+  START_KAVA_NODE=false
+fi
+
+if [ "$START_KAVA_NODE" = "true" ]
+then
+  mkdir -p /data/kava/config
+
+  case "$KAVA_NETWORK" in
+    kava-mainnet)
+      GENESIS_URL=https://kava-genesis-files.s3.amazonaws.com/kava_2222-10/genesis.json
+      ;;
+    kava-testnet)
+      GENESIS_URL=https://raw.githubusercontent.com/Kava-Labs/kava-testnets/master/16000/genesis.json
+      ;;
+    kava-9)
+      GENESIS_URL=https://kava-genesis-files.s3.amazonaws.com/kava-9/genesis.json
+      ;;
+    kava-8)
+      GENESIS_URL=https://kava-genesis-files.s3.amazonaws.com/kava-8-genesis-migrated-from-block-1878508.json
+      ;;
+    kava-7)
+      GENESIS_URL=https://raw.githubusercontent.com/Kava-Labs/launch/master/kava-7/genesis.json
+      ;;
+    kava-testnet-14000)
+      GENESIS_URL=https://raw.githubusercontent.com/Kava-Labs/kava-testnets/master/14000/genesis.json
+      ;;
+    kava-testnet-13000)
+      GENESIS_URL=https://raw.githubusercontent.com/Kava-Labs/kava-testnets/master/13000/genesis.json
+      ;;
+    kava-testnet-12000)
+      GENESIS_URL=https://raw.githubusercontent.com/Kava-Labs/kava-testnets/master/12000/genesis.json
+      ;;
+    *)
+      echo "unknown network"
+      exit 1
+      ;;
+  esac
+
+  echo "initializing config..."
+
+  if [ ! -f "/data/kava/config/app.toml" ]
+  then
+    cp "/app/templates/$KAVA_NETWORK/app.toml" /data/kava/config/app.toml
+  fi
+
+  if [ ! -f "/data/kava/config/config.toml" ]
+  then
+    cp "/app/templates/$KAVA_NETWORK/config.toml" /data/kava/config/config.toml
+  fi
+
+  if [ ! -f "/data/kava/config/genesis.json" ]
+  then
+    echo "downloading genesis..."
+    curl -s "$GENESIS_URL" -o /data/kava/config/genesis.json
+  fi
+fi
+
+supervisord -c /etc/supervisor/supervisord.conf
